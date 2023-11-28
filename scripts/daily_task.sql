6ccfef882d7df5b3fb6659d86d5664d3547c99ce
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-11-28 13:43:30 +0300

    vo

--- scripts/daily_task.sql
+++ scripts/daily_task.sql
@@ -2,15 +2,15 @@ INSERT INTO sttgaz.dm_isc_goods_remnants
 WITH sq AS(
     SELECT VIN
     FROM sttgaz.dm_isc_goods_remnants
-    WHERE DATE_TRUNC('MONTH', load_date) = DATE_TRUNC('MONTH', '{next_execution_date}'::date)
+    WHERE DATE_TRUNC('MONTH', load_date) = DATE_TRUNC('MONTH', '{{next_execution_date}}'::date)
 )
 SELECT *
 FROM sttgaz.stage_isc_goods_remnants
-WHERE load_date = '{next_execution_date}'
+WHERE load_date = '{{next_execution_date}}'
     AND VIN NOT IN (SELECT * FROM sq);
 
 DELETE FROM sttgaz.dm_isc_goods_remnants
-WHERE DATE_TRUNC('MONTH', load_date) = DATE_TRUNC('MONTH', '{next_execution_date}'::date)
+WHERE DATE_TRUNC('MONTH', load_date) = DATE_TRUNC('MONTH', '{{next_execution_date}}'::date)
     AND VIN NOT IN (SELECT VIN
                     FROM sttgaz.stage_isc_goods_remnants
-                    WHERE load_date = '{next_execution_date}');
+                    WHERE load_date = '{{next_execution_date}}');
--- scripts/monthly_task.sql
+++ scripts/monthly_task.sql
@@ -1,7 +1,7 @@
 DELETE FROM sttgaz.dm_isc_goods_remnants
-WHERE load_date = '{next_execution_date}';
+WHERE load_date = '{{next_execution_date}}';
 
 INSERT INTO sttgaz.dm_isc_goods_remnants
 SELECT *
 FROM sttgaz.stage_isc_goods_remnants
-WHERE load_date = '{next_execution_date}';
+WHERE load_date = '{{next_execution_date}}';
